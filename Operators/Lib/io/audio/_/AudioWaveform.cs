5cf1301a4d313cf968e777d2d90c55623bc75a4b
newemka <newemka@gmail.com>
2025-09-07 00:20:42 +0200

    fix conflict with AudioFrequencies

--- Operators/Lib/io/audio/_/AudioWaveform.cs
+++ Operators/Lib/io/audio/_/AudioWaveform.cs
@@ -5,7 +5,7 @@ using T3.Core.Utils;
 namespace Lib.io.audio._;
 
 [Guid("55E338F7-6D8C-4B61-8E10-9BB4D5FCFF91")]
-internal sealed class AudioWaveform : Instance<AudioFrequencies>
+internal sealed class AudioWaveform : Instance<AudioWaveform>
 {
     [Output(Guid = "AB0976EB-2B14-4AA2-A12C-398310A6E07B", DirtyFlagTrigger = DirtyFlagTrigger.Animated)]
     public readonly Slot<List<float>> Left = new();
