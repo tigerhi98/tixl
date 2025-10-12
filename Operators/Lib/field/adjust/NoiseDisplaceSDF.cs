8029a9b82900c461e0d480f26ed8f21b82b7896b
Thomas Mann <thomas.mann@framefield.com>
2025-10-12 21:57:44 +0200

    Update: [NoiseDisplaceSDF], [BendField] and [TwistTwist] now have a StepFactor parameter

--- Operators/Lib/field/adjust/NoiseDisplaceSDF.cs
+++ Operators/Lib/field/adjust/NoiseDisplaceSDF.cs
@@ -120,7 +120,10 @@ internal sealed class NoiseDisplaceSDF : Instance<NoiseDisplaceSDF>
     void IGraphNodeOp.GetPostShaderCode(CodeAssembleContext cac, int inputIndex)
     {
         var c = cac.ContextIdStack[^1];
-        cac.AppendCall($"f{c}.w += fSimplexNoiseDisplace(p{c}.xyz, {ShaderNode}Amount, {ShaderNode}Scale, -{ShaderNode}Offset);");
+        cac.AppendCall($"""
+                        f{c}.w += fSimplexNoiseDisplace(p{c}.xyz, {ShaderNode}Amount, {ShaderNode}Scale, -{ShaderNode}Offset);
+                        f{c}.w *= {ShaderNode}StepFactor; 
+                        """);
     }
 
     [Input(Guid = "1799f18f-92c5-4885-b6c1-6a196eee805f")]
@@ -137,4 +140,9 @@ internal sealed class NoiseDisplaceSDF : Instance<NoiseDisplaceSDF>
     [GraphParam]
     [Input(Guid = "6CD7B3CA-323C-4A69-989B-1B10009B8A90")]
     public readonly InputSlot<Vector3> Offset = new();
+    
+    [GraphParam]
+    [Input(Guid = "2D258B34-D45A-475E-987F-EA16CD4D5292")]
+    public readonly InputSlot<float> StepFactor = new();
+    
 }
